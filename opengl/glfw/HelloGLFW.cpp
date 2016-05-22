f656c479085e57595d5cc98a398af620110adb00
Eugene Mihailenco <mihailencoe@gmail.com>
2016-05-22 20:54:42 +0300

    Updated glfw example

--- opengl/glfw/HelloGLFW.cpp
+++ opengl/glfw/HelloGLFW.cpp
@@ -4,13 +4,36 @@
 #include <cassert>
 #include <thread>
 
-
 int main(void) {
 
     // Initialise GLFW
-    glfwInit();
+    if (!glfwInit())
+        return -1;
 
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+    //std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    // Create window
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", nullptr, nullptr); // Windowed
+    if (!window) 
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
 
     // Close OpenGL window and terminate GLFW
     glfwTerminate();
