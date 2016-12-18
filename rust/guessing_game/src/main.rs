9a62caf2d5e9916d12a569291eca0f50e5aa544d
Eugene Mihailenco <mihailencoe@gmail.com>
2016-12-18 22:01:59 +0200

    Playing with rust

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -6,8 +6,10 @@ use rand::Rng;
 use std::cmp::Ordering;
 
 
+// We return an empty tuple, not void as in C
 fn main() {
 
+    // Macros, uses ! to indicate that fact
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
@@ -18,10 +20,13 @@ fn main() {
 
         println!("Please input your guess.");
 
+        // here is a mutable variable binding
+        // String is growable utf8 string
+        // new() is associated function for String type
         let mut guess = String::new();
 
         io::stdin().read_line(&mut guess)
-            .ok()
+            // .ok()
             .expect("Failed to read line");
 
         let guess: u32 = match guess.trim().parse() {
